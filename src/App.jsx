b6e1adec4f2709036a9fe76985c36fc04b29e050
Mase <snoopythe3fans@hotmail.com>
2025-12-17 18:51:38 +0000

    doing some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,12 @@ import { Route } from "react-router-dom";
 import Header from "./components/header";
 import Home from "./pages/home";
 
+function App(){
+return(
 <routes>
     <Header />
     <Route path="/home" element={ Home } />
 </routes>
+);
+}
+export default App;
